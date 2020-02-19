9c15a78edf039be20d3cdfc336cfeb82b526be5b
Mark Karpelès <magicaltux@gmail.com>
2020-02-19 14:31:49 +0900

    move browserrouter to index

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<Router>
-					<Route exact path="/">
-						<h1>AZUSA</h1>
-					</Route>
-				</Router>
+				<Route exact path="/">
+					<h1>AZUSA</h1>
+				</Route>
 			</header>
 		</div>
 	);
