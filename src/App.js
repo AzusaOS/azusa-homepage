f719c5417d15fbb00bc577f4c1031ca978c0466b
Mark Karpelès <magicaltux@gmail.com>
2020-02-19 14:29:15 +0900

    route test 1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>AZUSA</h1>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<Router>
+					<Route exact path="/">
+						<h1>AZUSA</h1>
+					</Route>
+				</Router>
+			</header>
+		</div>
+	);
 }
 
 export default App;
