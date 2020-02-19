99fb279076fcd79aaf37172bc304537e8d0dc91f
Mark Karpelès <magicaltux@gmail.com>
2020-02-19 15:57:14 +0900

    ssr: wip

--- src/index.js
+++ src/index.js
@@ -1,58 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { Helmet } from "react-helmet";
 import * as serviceWorker from './serviceWorker';
-import { renderToString } from 'react-dom/server';
-import { BrowserRouter, StaticRouter } from "react-router-dom";
-import { getPrefix, getUuid, getUrl } from "@karpeleslab/klbfw";
+import { run } from "./ssr.js";
+
+run(<App/>);
 
 if(typeof window !== 'undefined') {
-	// in browser
-	ReactDOM.render(<BrowserRouter basename={getPrefix()}><App /></BrowserRouter>, document.getElementById('root'));
-
 	// If you want your app to work offline and load faster, you can change
 	// unregister() to register() below. Note this comes with some pitfalls.
 	// Learn more about service workers: https://bit.ly/CRA-PWA
 	serviceWorker.unregister();
 }
-
-global._renderToString = (cbk) => {
-	let result = { uuid: getUuid() };
-
-	try {
-		let context = {};
-
-		result.app = renderToString(
-			<StaticRouter context={context} basename={getPrefix()} location={getUrl().full}>
-				<App />
-			</StaticRouter>
-		);
-
-		if (context.status)
-			result.statusCode = context.status;
-
-		if (context.url) {
-			result.redirect = context.url;
-			cbk(result);
-			return;
-		}
-
-		// grab helmet
-		Helmet.canUseDOM = false;
-		const helmet = Helmet.renderStatic();
-
-		result.title = helmet.title ? helmet.title.toString() : null;
-		result.meta = helmet.meta ? helmet.meta.toString() : null;
-		result.script = helmet.script ? helmet.script.toString() : null;
-		result.link = helmet.link ? helmet.link.toString() : null;
-		result.bodyAttributes = helmet.bodyAttributes ? helmet.bodyAttributes.toString() : null;
-		result.htmlAttributes = helmet.htmlAttributes ? helmet.htmlAttributes.toString() : null;
-
-		cbk(result);
-	} catch(e) {
-		result.error = e;
-		cbk(result);
-	}
-};
---
+++ src/ssr.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Helmet } from "react-helmet";
+import { renderToString } from 'react-dom/server';
+import { BrowserRouter, StaticRouter } from "react-router-dom";
+import { getPrefix, getUuid, getUrl } from "@karpeleslab/klbfw";
+
+export function makeRenderer(app) {
+	return function(cbk) {
+		let result = { uuid: getUuid() };
+
+		try {
+			let context = {};
+
+			result.app = renderToString(
+				<StaticRouter context={context} basename={getPrefix()} location={getUrl().full}>
+					{app}
+				</StaticRouter>
+			);
+
+			if (context.status)
+				result.statusCode = context.status;
+
+			if (context.url) {
+				result.redirect = context.url;
+				cbk(result);
+				return;
+			}
+
+			// grab helmet
+			Helmet.canUseDOM = false;
+			const helmet = Helmet.renderStatic();
+
+			result.title = helmet.title ? helmet.title.toString() : null;
+			result.meta = helmet.meta ? helmet.meta.toString() : null;
+			result.script = helmet.script ? helmet.script.toString() : null;
+			result.link = helmet.link ? helmet.link.toString() : null;
+			result.bodyAttributes = helmet.bodyAttributes ? helmet.bodyAttributes.toString() : null;
+			result.htmlAttributes = helmet.htmlAttributes ? helmet.htmlAttributes.toString() : null;
+
+			cbk(result);
+		} catch(e) {
+			result.error = e;
+			cbk(result);
+		}
+	};
+};
+
+export function run(app) {
+	global._renderToString = makeRenderer(app);
+
+	if (typeof window !== 'undefined') {
+		ReactDOM.render(<BrowserRouter basename={getPrefix()}>{app}</BrowserRouter>, document.getElementById('root'));
+	}
+}
