263d5a3a34193d8408595ee61945304dc309cdf2
Mark Karpeles <magicaltux@gmail.com>
2020-02-21 00:03:45 +0900

    now using i18n

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React from 'react';
 import './App.css';
 import { Route } from "react-router-dom";
 import { Helmet } from "react-helmet";
+import { useTranslation } from "react-i18next";
 
 function App() {
+	const { t } = useTranslation();
+
 	return (
 		<div className="App">
 			<header className="App-header">
 				<Route exact path="/">
 					<Helmet>
 						<meta charSet="utf-8" />
-						<title>AZUSA</title>
-						<meta name="description" content="Todo" />
+						<title>{t("azusa_title")}</title>
+						<meta name="description" content={t("azusa_desc")} />
 					</Helmet>
 					<h1>AZUSA</h1>
 				</Route>
--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { run } from "@karpeleslab/react-klbfw-hooks";
+import { i18nPromise } from "./i18n";
 
-run(<App/>);
+run(<App/>, [i18nPromise]);
 
 if(typeof window !== 'undefined') {
 	// If you want your app to work offline and load faster, you can change
